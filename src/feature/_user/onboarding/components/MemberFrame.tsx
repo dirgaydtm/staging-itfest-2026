a902d957704bd40e178778e5098a9736699730c3
Weedanta <baguswedanta@student.ub.ac.id>
2025-06-23 23:37:55 +0700

    fix: adjust padding in MemberFrame component for better layout

--- src/feature/_user/onboarding/components/MemberFrame.tsx
+++ src/feature/_user/onboarding/components/MemberFrame.tsx
@@ -13,7 +13,7 @@ const MemberFrame: React.FC<MemberFrameProps> = ({ onBack }) => {
     router.push("/");
   };
   return (
-    <section className="flex flex-col items-center justify-center h-full w-full md:px-16 gap-12">
+    <section className="flex flex-col items-center justify-center h-full w-full md:px-8 lg:px-16 gap-12">
       <div className="text-center font-changa font-white flex flex-col items-center justify-center gap-4">
         <h3 className="font-bold text-2xl ">As a Team Member, great!</h3>
         <p className="text-md lg:text-lg text-center font-changa text-white">
