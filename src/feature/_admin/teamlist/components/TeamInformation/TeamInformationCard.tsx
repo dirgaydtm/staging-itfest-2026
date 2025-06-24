5ba0c9711b9330447638a74ce6b853c97e6635a0
afifalhauzan <afifalhauzan@student.ub.ac.id>
2025-06-24 22:41:50 +0700

    feat: enhance TeamInformationCard to handle null members

--- src/feature/_admin/teamlist/components/TeamInformation/TeamInformationCard.tsx
+++ src/feature/_admin/teamlist/components/TeamInformation/TeamInformationCard.tsx
@@ -5,6 +5,9 @@ interface TeamDetailsCardProps {
 }
 
 const TeamInformationCard = ({ teamInfo }: TeamDetailsCardProps) => {
+  // Create a default array of 2 empty members if members is null
+  const members = teamInfo.members || Array(2).fill({ full_name: null, student_number: null });
+
   return (
     <div className="p-8 bg-blue-500 rounded-4xl text-white border-2 border-purple-300 h-full">
       <h2 className="text-xl font-bold mb-2">Team Name</h2>
@@ -15,16 +18,16 @@ const TeamInformationCard = ({ teamInfo }: TeamDetailsCardProps) => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-4">
         <div>
           <h3 className="font-bold text-lg">Team Leader</h3>
-          <p className="text-gray-300">Name: {teamInfo.leader_name}</p>
-          <p className="text-gray-300">Student ID: {teamInfo.student_number}</p>
+          <p className="text-gray-300">Name: {teamInfo.leader_name || '-'}</p>
+          <p className="text-gray-300">Student ID: {teamInfo.student_number || '-'}</p>
         </div>
 
         {/* Mapping untuk anggota tim */}
-        {teamInfo.members.map((member, index) => (
+        {members.map((member, index) => (
           <div key={index}>
             <h3 className="font-bold text-lg">Team Member {index + 1}</h3>
-            <p className="text-gray-300">Name: {member.full_name}</p>
-            <p className="text-gray-300">Student ID: {member.student_number}</p>
+            <p className="text-gray-300">Name: {member.full_name || '-'}</p>
+            <p className="text-gray-300">Student ID: {member.student_number || '-'}</p>
           </div>
         ))}
       </div>
