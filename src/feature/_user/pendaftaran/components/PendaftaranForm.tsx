a29d4d6b9c64b870562e5a958f6fbc50286a2164
Weedanta <baguswedanta@student.ub.ac.id>
2025-06-24 14:12:41 +0700

    refactor: clean up imports and improve layout in PendaftaranForm and PageIndex components

--- src/feature/_user/pendaftaran/components/PendaftaranForm.tsx
+++ src/feature/_user/pendaftaran/components/PendaftaranForm.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import PageIndex from './PageIndex'
+import React from "react";
+import PageIndex from "./PageIndex";
+import { Button } from "@/shared/components/ui/Button";
 
 const PendaftaranForm = ({}) => {
   return (
-    <section className="flex flex-col items-center h-full md:mx-4 lg:mx-20 md:my-6 lg:my-12 ">
-        <PageIndex 
-        index={1}
-        title='Pilih Lomba'
-        />
-      
+    <section className="flex flex-col items-center justify-between h-full md:mx-4 lg:mx-20 md:py-6 lg:py-12 ">
+      <PageIndex index={1} title="Pilih Lomba" />
+      <Button
+        type="button"
+        size={"normal"}
+        className="w-full text-base sm:text-base disabled:opacity-50"
+      >
+        Pilih Lomba
+      </Button>     
     </section>
-  )
-}
+  );
+};
 
-export default PendaftaranForm
+export default PendaftaranForm;
