05e4e97d608c06fa97d8b9db78b17b3122ed7ea4
afifalhauzan <afifalhauzan@student.ub.ac.id>
2025-06-25 23:41:24 +0700

    merge auth update

--- src/feature/_admin/teamlist/container/TeamListContainer.tsx
+++ src/feature/_admin/teamlist/container/TeamListContainer.tsx
@@ -1,20 +1,31 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import TeamListTable from "../components/TeamListTable";
 import TeamListHeader from "../components/TeamListHeader";
 import { useParticipant } from "../../dashboard/hooks/useParticipantData";
 import { useTeamDetails } from "../hooks/useTeamDetailsData";
+import { Button } from "@/shared/components/ui/Button";
 
 const TeamListContainer = () => {
   const { totalAll } = useParticipant();
-  const { teamData } = useTeamDetails();
+  const { teamData, loading, error, refetch } = useTeamDetails();
+  const [currentFilter, setCurrentFilter] = useState("");
+
+  const handleFilterChange = (filter: string) => {
+    setCurrentFilter(filter);
+  };
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
-    <section className="px-4 sm:px-8 mycontainer md:px-12 lg:px-20 py-24 md:py-12 lg:py-20 h-full">
+    <section className="px-4 font-changa sm:px-8 mycontainer md:px-12 lg:px-20 py-24 md:py-12 lg:py-20 h-full">
       <div className="mb-8">
         <TeamListHeader />
       </div>
-      <TeamListTable totalAll={totalAll} teamData={teamData} />
+
+      <TeamListTable totalAll={totalAll} teamData={teamData} currentFilter={currentFilter} onFilterChange={handleFilterChange} />
     </section>
   );
 };
