ff1255f7e817aac73f8b93c04ced6dcb41b2f919
afifalhauzan <afifalhauzan@student.ub.ac.id>
2025-06-25 20:56:09 +0700

    fix: update payment status from "belum terverifikasi" to "diproses" in TeamsService and adjust styles for "ditolak" case

--- src/shared/utils/paymentStyles.ts
+++ src/shared/utils/paymentStyles.ts
@@ -3,9 +3,11 @@ export const getPaymentStatusStyle = (status: string) => {
         case "belum terverifikasi":
             return "px-2 py-1 rounded-md text-sm bg-slate-600 text-white";
         case "ditolak":
-            return "px-2 py-1 rounded-md text-sm bg-yellow-700 text-white";
+            return "px-2 py-1 rounded-md text-sm bg-red-700 text-white";
         case "terverifikasi":
             return "px-2 py-1 rounded-md text-sm bg-green-800 text-white";
+        case "diproses":
+            return "px-2 py-1 rounded-md text-sm bg-yellow-800 text-white";
         default:
             return "px-2 py-1 rounded-md text-sm bg-gray-200";
     }
