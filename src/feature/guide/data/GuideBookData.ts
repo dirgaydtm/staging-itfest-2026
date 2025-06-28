26794cb612b46c21ce813394963d8da538194217
syaquiii <msyauqioqi@gmail.com>
2025-06-28 14:58:24 +0700

    refactor : add uiux guidebook

--- src/feature/guide/data/GuideBookData.ts
+++ src/feature/guide/data/GuideBookData.ts
@@ -1,6 +1,6 @@
 import { StaticImageData } from "next/image";
-import BP from "@/assets/img/guide/guide.jpg";
-
+import BP from "@/assets/img/guide/BP.jpg";
+import UIUX from "@/assets/img/guide/UIUX.jpg";
 export interface GuideBookData {
   title: string;
   subtitle: string;
@@ -13,15 +13,15 @@ export const GuideBooks: GuideBookData[] = [
   {
     title: "Guidebook BP",
     subtitle: "Your Complete Blueprint to Success!",
-    href: "https://drive.google.com/file/d/1JeaQSNOMEQrLOINHXT7rWgXvJBi7RUD2/view?usp=sharing",
+    href: "https://drive.google.com/file/d/1JeaQSNOMEQrLOINHXT7rWgXvJBi7RUD2/view?usp=drive_link",
     glowColor: "from-cyan-400/20 to-blue-600/30",
     image: BP,
   },
   {
     title: "Guidebook UI/UX",
     subtitle: "Revolutionize Your Design Thinking!",
-    href: "https://drive.google.com/file/d/1JeaQSNOMEQrLOINHXT7rWgXvJBi7RUD2/view?usp=sharing",
+    href: "https://drive.google.com/file/d/1UfFWp_uh89dlbsAnH2Cs1CG5Twp8vzQC/view?usp=drive_link",
     glowColor: "from-purple-400/20 to-pink-600/30",
-    image: BP,
+    image: UIUX,
   },
 ];
