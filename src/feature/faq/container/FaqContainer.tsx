f1755060856009163d0e3ce1d24fd7cbc9c46d06
Weedanta <baguswedanta@student.ub.ac.id>
2025-06-29 03:09:32 +0700

    fix: adjust padding and font weight in FaqList and FaqContainer components

--- src/feature/faq/container/FaqContainer.tsx
+++ src/feature/faq/container/FaqContainer.tsx
@@ -9,7 +9,7 @@ const FaqContainer = () => {
         <h4 className="text-blue-100 mb-10 font-neighbor text-center 2xl:text-6xl lg:text-5xl text-3xl">
           Frequently Asked Question
         </h4>
-        <div className="grid grid-cols-1 gap-12 place-items-center">
+        <div className="grid grid-cols-1 gap-8 place-items-center">
           {faqdata.map((faq) => (
             <FaqList key={faq.id} faq={faq} />
           ))}
