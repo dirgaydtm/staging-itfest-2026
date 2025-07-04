1ff61bfdd2f2192158f0b1170c3149cab300971d
afifalhauzan <afifalhauzan@student.ub.ac.id>
2025-07-04 17:06:29 +0700

    fix: adjust padding in EventContainer for better layout

--- src/feature/event/container/EventContainer.tsx
+++ src/feature/event/container/EventContainer.tsx
@@ -5,7 +5,7 @@ import EventSeparator from "../components/EventSeparator";
 
 const EventContainer = () => {
   return (
-    <section className="eventbg min-h-[80px] py-40 -mt-10  z-10 relative">
+    <section className="eventbg min-h-[80px] py-20 md:py-25 -mt-10  z-10 relative">
       {/* sementara yahh */}
       {/* <Hero /> */}
       {/* <Sponsor /> */}
