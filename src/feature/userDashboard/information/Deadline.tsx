b4992315c9027ec19e138e5f146ea388bf70d9eb
Ammar Triwahyu <ammartriwahyu@gmail.com>
2026-06-08 12:21:05 +0700

    feat: update deadline view

--- src/feature/userDashboard/information/Deadline.tsx
+++ src/feature/userDashboard/information/Deadline.tsx
@@ -1,18 +1,20 @@
-import React from "react";
-import { useCountdown } from "../../hooks/useCountdown";
+"use client";
 
-interface Props {
+import { useCountdown } from "../hooks/useCountdown";
+
+type Props = {
+  title?: string;
   deadline: string;
-}
+};
 
-const Deadline = ({ deadline }: Props) => {
-  const timeLeft = useCountdown(deadline);
+const Deadline = ({ title = "Submission Deadline", deadline }: Props) => {
+  const t = useCountdown(deadline);
 
   return (
-    <section className="px-2 py-14 bg-blue-500 rounded-4xl text-white text-center border-2 border-purple-300 h-full">
-      <h2 className="font-changa font-bold text-3xl">Submission Deadline</h2>
-      <p className="font-robotech lg:text-5xl text-5xl mt-2 text-purple-100 lg:tracking-widest tracking-wide">
-        {`${timeLeft.days}:${timeLeft.hours}:${timeLeft.minutes}:${timeLeft.seconds}`}
+    <section className="h-full rounded-3xl bg-white/[0.06] backdrop-blur-lg border border-white/15 px-6 py-10 text-center font-leaguespartan text-light-blue">
+      <h2 className="text-2xl md:text-3xl font-bold mb-6">{title}</h2>
+      <p className="text-4xl md:text-5xl font-bold tracking-wider text-light-blue/60">
+        {t.days}: {t.hours}: {t.minutes}: {t.seconds}
       </p>
     </section>
   );
