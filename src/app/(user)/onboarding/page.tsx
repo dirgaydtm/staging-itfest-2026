bf2f5e576ee5f62798bdf328dd0e772e15b613ce
Ammar Triwahyu <ammartriwahyu@gmail.com>
2026-06-08 19:53:48 +0700

    fix: fix import temp

--- src/app/(user)/onboarding/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import OnboardingContainer from '@/feature/_user/onboarding/container/OnboardingContainer'
-
-const page = () => {
-  return <OnboardingContainer />
-}
-
-export default page
