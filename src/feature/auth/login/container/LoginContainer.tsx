85a47a7a5c78ac8705a17959e2bb621fadca24cd
REMITMID <sanders060806@gmail.com>
2026-06-18 12:08:55 +0700

    feat: add elipse in LoginContainer.tsx

--- src/feature/auth/login/container/LoginContainer.tsx
+++ src/feature/auth/login/container/LoginContainer.tsx
@@ -27,33 +27,31 @@ const LoginContainer = () => {
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[300px] md:w-[600px] h-[300px] md:h-[600px] bg-black rounded-full blur-[100px] md:blur-[150px] opacity-60"></div>
       <div className="absolute inset-0 z-0 pointer-events-none">
         
-        {/* Bintang Kiri Bawah (Merah) */}
         <Image 
           src={BintangLeft} 
           alt="Hiasan Kiri" 
           className="absolute left-0 opacity-80"
         />
 
-        {/* Bintang Kanan Atas (Putih) */}
         <Image 
           src={BintangRight} 
           alt="Hiasan Kanan" 
           className="absolute right-0 opacity-80"
         />
 
-        {/* Union Kiri (Kecil) */}
         <Image 
           src={UnionLeft} 
           alt="Union Kiri" 
           className="absolute left-0 bottom-20"
         />
 
-        {/* Union Kanan (Kecil) */}
         <Image 
           src={UnionRight} 
           alt="Union Kanan" 
           className="absolute right-0 top-24"
         />
+
+        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 w-[800px] md:w-[1600px] h-[200px] md:h-[150px] bg-normal-hover-blue rounded-[100%] blur-[100px] md:blur-[120px]"></div>
         
       </div>
       <LoginForm
