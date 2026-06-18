97cf2e714b1a04c0cd5085d42f52c037e083a2f6
REMITMID <sanders060806@gmail.com>
2026-06-18 12:26:31 +0700

    feat: add BackgroundAuth in ForgotPasswordContainer.tsx

--- src/feature/auth/forgot-password/container/ForgotPasswordContainer.tsx
+++ src/feature/auth/forgot-password/container/ForgotPasswordContainer.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { ForgotPasswordForm } from "../components/ForgotPasswordForm";
+import BackgroundAuth from "@/shared/components/layout/BackgroundAuth";
 
 const ForgotPasswordContainer = () => {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center overflow-hidden bg-gradient-to-b from-slate-900 to-indigo-900 relative font-changa">
+    <main className="min-h-screen flex flex-col items-center justify-center overflow-hidden bg-darker-blue relative font-changa">
+      <BackgroundAuth />
       <ForgotPasswordForm />
     </main>
   );
