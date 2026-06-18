70ab5845cddaef61a1099f4c1bc5eec5fac8776f
REMITMID <sanders060806@gmail.com>
2026-06-18 12:23:52 +0700

    feat: add BackgroundAuth fot RegisterContainer

--- src/feature/auth/register/container/RegisterContainer.tsx
+++ src/feature/auth/register/container/RegisterContainer.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import RegisterForm from "../components/RegisterForm";
+import BackgroundAuth from "@/shared/components/layout/BackgroundAuth";
 
 const RegisterContainer = () => {
   return (
-    <section className="min-h-screen flex flex-col items-center justify-center overflow-hidden bg-gradient-to-b from-slate-900 to-indigo-900 relative">
+    <section className="min-h-screen flex flex-col items-center justify-center overflow-hidden bg-darker-blue relative">
+      <BackgroundAuth />
       <RegisterForm />
     </section>
   );
